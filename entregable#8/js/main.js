5ab430d90f2235bce9727cdd69e118b4dfd90f95
jchaverra-root <88665755+jchaverra-root@users.noreply.github.com>
2022-09-13 08:57:19 -0500

    Delete entregable#8 directory

--- entregable#8/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("keydown", (e) => {
-    console.log(e.type);
-    console.log(e.code);
-    console.log(e);
-    if (e.code == "Enter") {
-        window.location.href = 'deportista.html'
-    }
-});
